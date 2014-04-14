41310789a00a4dcee1e425b86f57ce561a4058c7
Drew Brisbin <brisbind@gmail.com>
2014-04-14 18:14:46 -0400

    Added trivial helloworld program for Git tutorial

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# Copyright [Drew who likes oranges]
+
+name="Drew"
+favoritething="oranges"
+
+print "Hello world."
+print "My name is "+name
+print "and I like "+favoritething
